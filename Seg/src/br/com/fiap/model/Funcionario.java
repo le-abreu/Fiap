1bc760109f89b465b5bd6ec8737eeddbbcba8449
le-abreu <LeulSeixas@LeulSeixas-PC>
2012-10-05 00:20:06 -0300

    Commit JPA

---
+++ Seg/src/br/com/fiap/model/Funcionario.java
@@ -0,0 +1,18 @@
+package br.com.fiap.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Funcionario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	@Column
+	private String nome;
+}
