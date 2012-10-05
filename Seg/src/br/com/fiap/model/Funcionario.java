edd51200853d2377e59719cac908e38e6a3f3d14
le-abreu <le_abreu@live.com>
2012-10-05 11:56:45 -0300

    #1: Configurando Ambiente

--- Seg/src/br/com/fiap/model/Funcionario.java
+++ Seg/src/br/com/fiap/model/Funcionario.java
@@ -1,5 +1,7 @@
 package br.com.fiap.model;
 
+import java.util.Calendar;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,11 +10,95 @@ import javax.persistence.Id;
 
 @Entity
 public class Funcionario {
-
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private long id;
+	private long chapa;
 
 	@Column
 	private String nome;
+
+	@Column
+	private String sobrenome;
+
+	@Column
+	private Calendar dataNascimento;
+	
+	@Column
+	private Calendar dataAdmissao;
+	
+	@Column
+	private Calendar dataUltimaPromocao;
+	
+	@Column
+	private String usuario;
+	
+	@Column
+	private String chavePublica;
+
+	public long getChapa() {
+		return chapa;
+	}
+
+	public void setChapa(long chapa) {
+		this.chapa = chapa;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	public Calendar getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Calendar dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public Calendar getDataAdmissao() {
+		return dataAdmissao;
+	}
+
+	public void setDataAdmissao(Calendar dataAdmissao) {
+		this.dataAdmissao = dataAdmissao;
+	}
+
+	public Calendar getDataUltimaPromocao() {
+		return dataUltimaPromocao;
+	}
+
+	public void setDataUltimaPromocao(Calendar dataUltimaPromocao) {
+		this.dataUltimaPromocao = dataUltimaPromocao;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getChavePublica() {
+		return chavePublica;
+	}
+
+	public void setChavePublica(String chavePublica) {
+		this.chavePublica = chavePublica;
+	}
+	
 }
+ 
